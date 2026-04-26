a5e3b720f589118b21f42e824e48dc2b01ae3a78
charleengulo <charleengulo@student.ub.ac.id>
2026-04-26 22:44:53 +0700

    Delete kohisop directory

--- kohisop/src/Currency.java
+++
@@ -1,9 +0,0 @@
-public interface Currency {
-
-  String getName();
-  String getSymbol();
-  double getExchangeRateToIDR();
-  double convert(double idrThousands);
-  String format(double idrThousands);
-  
-}
